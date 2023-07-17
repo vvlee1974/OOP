9a0641508ee8b00e8acc08a4fb32eb24ab60bfc5
vvlee <vvlee@gmail.com>
2023-07-17 20:54:55 +0500

    change lesson6-7.classwork

--- src/main/java/org/example/lesson6/classwork/ex601/genre/Genre.java
+++ src/main/java/org/example/lesson6/classwork/ex601/genre/Genre.java
@@ -1,5 +1,5 @@
 package org.example.lesson6.classwork.ex601.genre;
 
 public interface Genre {
-    public String toGenre();
+    String toGenre();
 }
--- src/main/java/org/example/lesson6/classwork/ex601/genre/GenreBook.java
+++ src/main/java/org/example/lesson6/classwork/ex601/genre/GenreBook.java
@@ -1,12 +1,14 @@
 package org.example.lesson6.classwork.ex601.genre;
 
-import org.example.lesson6.classwork.ex601.genre.Genre;
 
 public class GenreBook implements Genre {
     private String genreName;
 
     public GenreBook(String genreName) {
         this.genreName = genreName;
+    }
+
+    public GenreBook() {
     }
 
     @Override
