b117b3bae9dc43992f29c1888780df486e6ece0d
vvlee <vvlee@gmail.com>
2023-07-19 00:49:22 +0500

    change finalWorkOOP

--- src/main/java/org/example/lesson7/finalWork/Main.java
+++ src/main/java/org/example/lesson7/finalWork/Main.java
@@ -9,9 +9,20 @@ public class Main {
         ComplexNumber b = new ComplexNumber(3.0, 2.0);
 
         Controller controller = new Controller();
+        System.out.println("================================");
+        System.out.println("ДЕЙСТВИЯ С КОМПЛЕКСНЫМИ ЧИСЛАМИ");
+        System.out.println("================================");
+        System.out.println();
+        System.out.println("Сложение комплексных чисел");
         System.out.println("(a + b) = " + controller.sum(a, b));
+        System.out.println("----------------------------");
+        System.out.println("Вычитание комплексных чисел");
         System.out.println("(a - b) = " + controller.sub(a, b));
+        System.out.println("----------------------------");
+        System.out.println("Умножение комплексных чисел");
         System.out.println("(a * b) = " + controller.mul(a, b));
+        System.out.println("----------------------------");
+        System.out.println("Деление комплексных чисел");
         System.out.println("(a / b) = " + controller.div(a, b));
     }
 }
